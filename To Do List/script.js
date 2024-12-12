0b78fab14bc063ba25eaae44b457dcbb794de44c
kkcaste <150423975+kkcaste@users.noreply.github.com>
2024-12-12 11:33:20 +0100

    Create script.js

---
+++ To Do List/script.js
@@ -0,0 +1,38 @@
+const list=document.getElementById("list");
+let array = [];
+let holders = ["Pasta","Broccoli","Milk","Coca-Cola","Cereals","Cookies","Water"];
+
+	function add(){
+		let randNum=Math.floor(Math.random()*holders.length);
+	
+		let uInput=document.getElementById("usrInput");
+		if(uInput.value==""){
+			window.alert("You have to write something to add!!");
+		}else{
+			array.push(uInput.value);
+			console.log(array);
+			uInput.value="";
+			uInput.placeholder="Es. "+holders[randNum];
+				for( i=array.length-1;i<array.length;i++){	
+					let li = document.createElement('li');
+					li.innerText = array[i];
+					list.appendChild(li);
+				}	
+		}
+	}
+    
+    function sub(){
+    	if(array==""){
+        	window.alert("You have nothing to remove!");
+        }
+		let uInput=document.getElementById("usrInput");
+        var listItems = list.getElementsByTagName("li");
+        array.pop();
+		var last=listItems[listItems.length-1];
+    	list.removeChild(last);
+	}
+
+	
+  
+
+
